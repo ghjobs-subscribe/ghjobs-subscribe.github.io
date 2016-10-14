15de422417f885689b8e1a72f6490783e2aada2e
Hasit Mistry <hasitnm@gmail.com>
2016-10-14 12:37:47 -0700

    Do some fake loading before showing message

--- js/index.js
+++ js/index.js
@@ -20,9 +20,12 @@ function showError() {
 
 function processJSON(data) {
     if (data.success === true) {
-        $(".form").hide();
-    }
-    $(".spinner").hide();
-    $(".message").text(data.message);
-    $(".message").show();
+            $(".message").hide();
+            $(".form").hide();
+        }
+    setTimeout(function () {
+        $(".spinner").hide();
+        $(".message").text(data.message);
+        $(".message").show();
+    }, 1000);
 }
