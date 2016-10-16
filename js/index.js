8c46c168befa518b80ec4fbd0eb63e5f81c9b3d1
Hasit Mistry <hasit@ubuntu-512mb-sfo2-01.localdomain>
2016-10-16 18:15:23 +0000

    Handle local api call

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     var options = {
+	url: 'https://api.ghjobssubscribe.com/subscribe',
+	type: 'post',
         dataType: 'json',
         success: processJSON,
         error: showServerError
