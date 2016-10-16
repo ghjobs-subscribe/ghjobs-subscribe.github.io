70790ebbc18318d331e2c35c9b7edd47dc91d391
Hasit Mistry <hasitnm@gmail.com>
2016-10-15 21:59:34 -0700

    Update action url

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
     var options = {
         dataType: 'json',
-        beforeSubmit: showSpinner,
         success: processJSON,
-        error: showError
+        error: showServerError
     };
     $('#subscribeUser').ajaxForm();
     $('#subscribeUser').submit(function () {
-        $(this).ajaxSubmit(options); 
+        $(this).ajaxSubmit(options);
         return false;
     });
 });
@@ -16,9 +15,8 @@ function showSpinner() {
     $(".spinner").show();
 }
 
-function showError() {
+function showServerError() {
     var message = "There seems to be an error with the server. Please come back later."
-    $(".spinner").hide();
     $(".message").text(message);
     $(".message").show();
 }
@@ -28,9 +26,6 @@ function processJSON(data) {
         $(".message").hide();
         $(".form").hide();
     }
-    setTimeout(function () {
-        $(".spinner").hide();
-        $(".message").text(data.message);
-        $(".message").show();
-    }, 1000);
+    $(".message").text(data.message);
+    $(".message").show();
 }
